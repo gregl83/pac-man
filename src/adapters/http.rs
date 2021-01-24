8f4dc746763a15af5c23a2ede1b33ea6a04d4721
Gregory Langlais <general@gregorylanglais.com>
2021-01-24 17:06:32 -0500

    add headers to http get_stream

--- src/adapters/http.rs
+++ src/adapters/http.rs
@@ -1,16 +1,17 @@
 use hyper::{
-    Body,
     Client,
+    HeaderMap,
+    Body,
 };
 use hyper_tls::HttpsConnector;
 use http::Request;
 
-pub async fn get_stream() -> Body {
+pub async fn get_stream() -> (HeaderMap, Body) {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, Body>(https);
 
     let request = Request::get("http://httpbin.org/ip").body(Body::empty()).unwrap(); // fixme - dynamic request
     let response = client.request(request).await.unwrap();
 
-    response.into_body()
+    (response.headers().clone(), response.into_body())
 }
--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .init()
         .unwrap();
 
-    let mut body = http::get_stream().await;
+    let (headers, mut body) = http::get_stream().await;
 
     while let Some(chunk) = body.next().await {
         println!("{:?}", chunk);
@@ -46,14 +46,6 @@ async fn my_handler(e: CustomEvent, c: Context) -> Result<Data, HandlerError> {
     // if e.id == "" {
     //     error!("Empty id in request {}", c.aws_request_id);
     //     bail!("Empty id");
-    // }
-
-    // let mut res = String::new();
-    // let stream = http::get_stream();
-    //
-    // while let Some(chunk) = stream.await {
-    //     res.push_str(&chunk?);
-    //     //stdout().write_all(&chunk?).await?;
     // }
 
     Ok(Data {
